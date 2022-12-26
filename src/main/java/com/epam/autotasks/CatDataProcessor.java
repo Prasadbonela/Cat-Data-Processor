0e32b5284205176c71be6f555a0dcfa55cc90d29
Sofia Krylova <sofia_krylova@epam.com>
2022-12-26 14:49:00 +0100

    Initial

---
+++ src/main/java/com/epam/autotasks/CatDataProcessor.java
@@ -0,0 +1,16 @@
+package com.epam.autotasks;
+
+import com.google.common.collect.ImmutableTable;
+import java.util.List;
+import org.json.JSONArray;
+
+public class CatDataProcessor {
+
+    public ImmutableTable<String, Cat.Breed, Integer> createCatTable(List<Cat> cats) {
+        return null;
+    }
+
+    public JSONArray createCatJson(List<Cat> cats) {
+        return null;
+    }
+}
